c3746b3cea7bf540e1bd668c61b9f051297e5f72
30209093 <alisha.tasleem1@ucalgary.ca>
2025-04-03 10:20:20 -0600

    Styled stock page and added ingredient status coloring

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='dashboard-login'),
     path('staff/', views.staff, name='dashboard-staff'),
-    path('manager/', views.manager, name='dashboard-manager')
+    path('manager/', views.manager, name='dashboard-manager'),
+    path('stock/', views.stock, name='stock-page')
 ]
