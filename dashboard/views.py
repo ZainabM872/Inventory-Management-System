3c83af3f4fcc1e682823439c86f00cf1815cc2bc
ZainabM872 <zainabmajid2408@gmail.com>
2025-04-16 11:20:29 -0600

    working on connecting frontend login to db

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # when running the server, this will be the initial page
 def index(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        userID = request.POST.get('user_id')
+        user = authenticate(request, username=username, password=password, user_id=userID)
+        
+        # authenticate function checks if the user is valid
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard/login.html')
+        else:
+            return redirect('dashboard/login.html', {
+                'error': 'Invalid username or password'
+            })
+
     return render(request, 'dashboard/login.html')
+
 
 # if we add '/staff' to the url of the page, it redirects to staff page
 def staff(request): 
