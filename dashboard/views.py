65358cd3002ec26aec6f3dc2559c05ced6abd2f0
ZainabM872 <zainabmajid2408@gmail.com>
2025-04-17 00:48:23 -0600

    worked on fixing the logic

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,18 +14,14 @@ def index(request):
         user = User.objects.filter(name=username, password=password).first()
         # authenticate function checks if the user is valid
         if user:
-        '''if user is not None:
-            login(request, user)
-            if(user.is_staff):
-                return redirect('dashboard/staff.html')
-            elif(user.is_manager):
-                return redirect('dashboard/manager.html')
-            #return redirect('dashboard/login.html')
+            if Staff.objects.filter(user=user).exists():
+                return redirect('dashboard-staff')
+            elif Manager.objects.filter(user=user).exists():
+                return redirect('dashboard-manager')
         else:
-            messages.error(request, 'Invalid username or password')
-            return redirect('dashboard-login')'''
-
-    return render(request, 'dashboard/login.html')
+            messages.error(request, 'Invalid username or password.')
+            return redirect('dashboard-login')
+    return render(request, 'dashboard/login.html') # otherwise if its a get request or user is not valid, it will render the login page
 
 
 # if we add '/staff' to the url of the page, it redirects to staff page
