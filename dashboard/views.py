1415821fe2c8326fb6695cd209f952e79e46f90d
ZainabM872 <zainabmajid2408@gmail.com>
2025-04-21 05:12:30 -0600

    working on linking the schedule to the schedule table in db

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
+import json
 from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 
 from dashboard.models import *
@@ -24,9 +25,36 @@ def index(request):
     return render(request, 'dashboard/login.html') # otherwise if its a get request or user is not valid, it will render the login page
 
 
+def get_logged_in_staff(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(name=username, password=password).first()
+        if user:
+            # return staff object, not just exists()
+            staff = Staff.objects.filter(user=user).first()
+            return staff
+    return None
 # if we add '/staff' to the url of the page, it redirects to staff page
 def staff(request): 
-    return render(request, 'dashboard/staff.html')
+    #staff_user = user()
+    userStaff = user()
+    schedule = get_object_or_404(Schedule, staff=userStaff)
+
+    #staffName = Schedule.objects.filter(staff=userStaff) # get the staff name from schedule 
+    shift_data = {
+        "tuesday": schedule.tuesday,
+        "wednesday": schedule.wednesday,
+        "thursday": schedule.thursday,
+        "friday": schedule.friday,
+        "saturday": schedule.saturday,
+        "sunday": schedule.sunday,
+        "staff_name": schedule.staff.name,
+    }
+    return render(request, "schedule.html", {
+        "shift_data_json": json.dumps(shift_data)
+    })
 
 # if we add '/manager' to the url of the page, it redirects to manager page
 def manager(request):
